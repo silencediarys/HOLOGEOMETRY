6859c654ca11f643798ca7a5f0ed896a28eefbfd
Pbalazs101 <42806085+Pbalazs101@users.noreply.github.com>
2022-03-16 19:37:20 +0100

    Naavigation and timer done up to 1st question. WIP

--- HoloGeometry/Assets/Scripts/TYKSecondScreenHandler.cs
+++ HoloGeometry/Assets/Scripts/TYKSecondScreenHandler.cs
@@ -2,16 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TYKSecondScreenHandler : MonoBehaviour
 {
 
+  public GameObject Question1;
+
   float timeLeft = 5;
 
-    // Start is called before the first frame update
-    void Start()
+    public void OpenQuestion1()          
     {
-        
+      if(Question1 != null)
+      {
+        Question1.SetActive(true);
+      }
     }
 
     // Update is called once per frame
@@ -20,11 +25,9 @@ public class TYKSecondScreenHandler : MonoBehaviour
       
       System.Threading.Thread.Sleep(1000);
       timeLeft -= 1;
-      if(timeLeft == 0)
+      if(timeLeft <= 0)
       {
-        print("Time's up!");
-
-
+          OpenQuestion1();
       }
       GameObject.Find("TimerButton").GetComponentInChildren<Text>().text = string.Format("Timer: {0}", timeLeft);
     }
