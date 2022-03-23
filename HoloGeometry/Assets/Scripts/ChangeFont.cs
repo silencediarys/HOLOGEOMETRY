e575e39bf65532518937cf2a625adf303a0c3fe7
bbacani <bernard.bacani@gmail.com>
2022-03-23 01:06:34 +0100

    Added Dyslexic Font option

---
+++ HoloGeometry/Assets/Scripts/ChangeFont.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeFont : MonoBehaviour {
+
+    public int chFont;
+    Toggle m_Toggle;
+
+    // Use this for initialization
+    void Start () {
+        //GetComponent<Button>().onClick.AddListener(FontChange);
+        //Fetch the Toggle GameObject
+        m_Toggle = GetComponent<Toggle>();
+        //Add listener for when the state of the Toggle changes, to take action
+        m_Toggle.onValueChanged.AddListener(delegate {
+            ToggleValueChanged(m_Toggle);
+        });
+        m_Toggle.isOn = false;
+    }
+
+    //Output the new state of the Toggle into Text
+    void ToggleValueChanged(Toggle change)
+    {
+        chFont = m_Toggle.isOn ? 1 : 0;
+        PlayerPrefs.SetInt("ChFont", chFont);
+        PlayerPrefs.Save();
+    }
+}
---
+++ HoloGeometry/Assets/Scripts/UpdateFont.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateFont : MonoBehaviour {
+
+    Text m_text;
+    public int _fontSizeD;
+    public int _fontSizeR;
+    public Font _regular;
+    public Font _dyslexic;
+    public int f;
+
+
+	// Use this for initialization
+	void Start () {
+        f = PlayerPrefs.GetInt("ChFont");
+        m_text = GetComponent<Text>();
+        if (f == 1)
+        {
+            m_text.font = _dyslexic;
+            m_text.fontSize = _fontSizeD;
+        }
+        else
+        {
+            m_text.font = _regular;
+            m_text.fontSize = _fontSizeR;
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        f = PlayerPrefs.GetInt("ChFont");
+        m_text = GetComponent<Text>();
+        if (f == 1)
+        {
+            m_text.font = _dyslexic;
+            m_text.fontSize = _fontSizeD;
+        }
+        else
+        {
+            m_text.font = _regular;
+            m_text.fontSize = _fontSizeR;
+        }
+
+    }
+}
